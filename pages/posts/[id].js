9c015e179611c82efaed9e9e1fa0287fbcd798db
luke-h1 <luke.howsam@yahoo.com>
2021-01-23 23:24:51 +0000

    updates

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,49 +1,44 @@
-import { API } from "aws-amplify";
-import { useRouter } from "next/router";
-import ReactMarkdown from "react-markdown";
-import { listPosts, getPost } from "../../graphql/queries";
+import { API } from 'aws-amplify'
+import { useRouter } from 'next/router'
+import ReactMarkdown from 'react-markdown'
+import '../../configureAmplify'
+import { listPosts, getPost } from '../../graphql/queries'
 
 export default function Post({ post }) {
-  const router = useRouter();
+  const router = useRouter()
   if (router.isFallback) {
-    return <div>Loading...</div>;
+    return <div>Loading...</div>
   }
   return (
     <div>
-      <h1 className="text-5xl mt-4 font-semibold tracking-wide">
-        {post.title}
-      </h1>
-      <p className="text-sm font-light my-4">By {post.username}</p>
+      <h1 className="text-5xl mt-4 font-semibold tracking-wide">{post.title}</h1>
+      <p className="text-sm font-light my-4">by {post.username}</p>
       <div className="mt-8">
-        <ReactMarkdown className="prose" children={post.content} />
+        <ReactMarkdown className='prose' children={post.content} />
       </div>
     </div>
-  );
+  )
 }
 
 export async function getStaticPaths() {
   const postData = await API.graphql({
-    query: listPosts,
-  });
-  const paths = postData.data.listPosts.items.map((post) => ({
-    //   { params: { id: post.id }}
-    params: { id: post.id },
-  }));
+    query: listPosts
+  })
+  const paths = postData.data.listPosts.items.map(post => ({ params: { id: post.id }}))
   return {
     paths,
-    fallback: true,
-  };
+    fallback: true
+  }
 }
 
-export async function getStaticProps({ params }) {
-  const { id } = params;
+export async function getStaticProps ({ params }) {
+  const { id } = params
   const postData = await API.graphql({
-    query: getPost,
-    variables: { id },
-  });
+    query: getPost, variables: { id }
+  })
   return {
     props: {
-      post: postData.data.getPost,
-    },
-  };
-}
+      post: postData.data.getPost
+    }
+  }
+} 
