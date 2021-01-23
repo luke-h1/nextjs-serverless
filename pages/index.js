5f6edf742f4dd1b724df84ee22ce0a18ec5d183b
luke-h1 <luke.howsam@yahoo.com>
2021-01-23 22:05:32 +0000

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+import Link from "next/link";
+import { API } from "aws-amplify";
+import { listPosts } from "../graphql/queries";
+
+export default function Home() {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+  async function fetchPosts() {
+    const postData = await API.graphql({
+      query: listPosts,
+    });
+    setPosts(postData.data.listPosts.items);
+  }
+  return (
+    <div>
+      <h1 className="text-3xl font-semibold tracking-wide mt-6 mb-2">Posts</h1>
+      {posts.map((post, i) => (
+        <Link key={i} href={`/posts/${post.id}`}>
+          <div className="cursor-pointer border-b border-gray-300 mt-8 pb-4">
+            <h2 className="text-xl font-semibold">{post.title}</h2>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
+}
